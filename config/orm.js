d95302ccbe76af1350a5a2be06cbd61c08f7c17c
Stephen Pino <spino725@gmail.com>
2017-04-28 00:23:04 -0500

    add ORMs

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,72 @@
 var connection = require("./connection.js");
 
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        if (Object.hasOwnProperty.call(ob, key)) {
+            arr.push(key + "=" + ob[key]);
+        }
+    }
+
+    return arr.toString();
+}
+
 var orm = {
-    selectAll: (tableInput) => {
-        var queryString = "SELECT * FROM ??";
-        connection.query(queryString, [tableInput], (err, result) => {
-            console.log(result);
+    selectAll: (tableInput, cb) => {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
     },
-    insertOne: (something) => {
-        var queryString = "";
-        connection.query(queryString, [], (err, result) => {
-            console.log(result);
+    insertOne: (table, cols, vals, cb) => {
+        var queryString = "INSERT INTO" + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
     },
-    updateOne: (something) => {
-        var queryString = "";
-        connection.query(queryString, [], (err, result) => {
-            console.log(result);
+    updateOne: (table, objColVals, condition, cb) => {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
     }
 
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var burger = require("../models/cat.js");
+var burger = require("../models/burgers.js");
 
 // Here will be all the routes and the logic within each individual route as needed
 
